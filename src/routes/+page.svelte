ce8b4e1df2e4cba5de8ef1a9a48c458667071b88
Mouzone <sunnyliu010@gmail.com>
2025-10-14 13:45:07 -0400

    Generate basic structure and layout for the html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div>
+	<p>Press the button to generate a random book of the bible</p>
+	<button>Spin</button>
+</div>
+
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+	div {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
