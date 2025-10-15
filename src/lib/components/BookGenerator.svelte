184b92ae8eda0001a89be3cacde2e35927b13248
Mouzone <sunnyliu010@gmail.com>
2025-10-15 16:14:41 -0400

    Add toggle to choose whether to generate from all books or just unread books

--- src/lib/components/BookGenerator.svelte
+++ src/lib/components/BookGenerator.svelte
@@ -1,27 +1,49 @@
 <script>
 	import data from "$lib/bible-data.json";
+	import { db } from "$lib/db";
+	import { liveQuery } from "dexie";
 
+	const readingProgress = liveQuery(() =>
+		db.readingProgress.where("dateRead").equals("").toArray()
+	);
+	let useUnreadBooks = $state(false);
 	let book = $state("");
 
 	function getRandomBook() {
-		const randomIndex = Math.floor(Math.random() * data.length);
-		book = data[randomIndex].bookName;
+		if (useUnreadBooks) {
+			const randomIndex = Math.floor(
+				Math.random() * $readingProgress.length
+			);
+			book = $readingProgress[randomIndex].book;
+		} else {
+			const randomIndex = Math.floor(Math.random() * data.length);
+			book = data[randomIndex].bookName;
+		}
 	}
 </script>
 
 <div class="generator-container">
-	<p class="result-display">
-		{#if book}
-			{book}
-		{:else}
-			Nothing
-		{/if}
-	</p>
-
-	<button
-		class="spin-button"
-		onclick={() => getRandomBook()}>CAST LOT</button
-	>
+	{#if $readingProgress}
+		<p class="result-display">
+			{#if book}
+				{book}
+			{:else}
+				Nothing
+			{/if}
+		</p>
+		<button
+			class="spin-button"
+			onclick={() => getRandomBook()}>CAST LOT</button
+		>
+	{/if}
+	<label class="switch">
+		<input
+			type="checkbox"
+			id="generateFromUnread"
+			bind:checked={useUnreadBooks}
+		/>
+		<span class="toggle-text">Only unread books</span>
+	</label>
 </div>
 
 <style>
