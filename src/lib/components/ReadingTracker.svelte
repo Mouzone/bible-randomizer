7c97428e621b4f911bb03efc8ebc2a1794f281b7
Mouzone <sunnyliu010@gmail.com>
2025-10-15 14:42:04 -0400

    Add functions to update read stateus and the proper inputs to trigger it

--- src/lib/components/ReadingTracker.svelte
+++ src/lib/components/ReadingTracker.svelte
@@ -3,10 +3,16 @@
 	import { db } from "$lib/db";
 
 	const readingProgressStore = liveQuery(() => db.readingProgress.toArray());
-
-	let i = $state(0);
 	const readingProgress = $derived($readingProgressStore ?? []);
-	const selectedProgress = $derived(readingProgress.at(i));
+
+	async function markRead(index, date) {
+		await db.readingProgress.update(index, { dateRead: date });
+	}
+
+	async function clearRead(index) {
+		await db.readingProgress.update(index, { dateRead: "" });
+	}
+	let i = $state(0);
 </script>
 
 {#if readingProgress.length > 0}
@@ -15,8 +21,11 @@
 			<option value={j}>{progress.book}</option>
 		{/each}
 	</select>
-
-	<div>Date Read: {selectedProgress?.dateRead}</div>
+	<input
+		type="date"
+		value={readingProgress.at(i)?.dateRead}
+	/>
+	<button onclick={() => clearRead(i)}> x</button>
 {:else}
 	<div>Loading reading progress...</div>
 {/if}
