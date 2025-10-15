65b6675897ac3ab688e6f302eaeeb4143058d585
Mouzone <sunnyliu010@gmail.com>
2025-10-15 14:21:33 -0400

    Create component to show some books

---
+++ src/lib/components/ReadingTracker.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { liveQuery } from "dexie";
+	import { db } from "$lib/db";
+
+	let readingProgress = liveQuery(() => db.readingProgress.toArray());
+</script>
+
+<ul>
+	{#if $readingProgress}
+		{#each $readingProgress as progress (progress.id)}
+			<li>{progress.book}</li>
+		{/each}
+	{/if}
+</ul>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import BookGenerator from "$lib/components/BookGenerator.svelte";
 	import ChapterGenerator from "$lib/components/ChapterGenerator.svelte";
+	import ReadingTracker from "$lib/components/ReadingTracker.svelte";
 	let componentToShow = $state("book");
 </script>
 
